3d241625ec3590dd8be680de04da992afe612a6d
Michel Castro <42982212+mixchel@users.noreply.github.com>
2022-03-18 14:32:46 -0300

    Terminado sem substiuir os caracteres pelos valores

--- TP2.v
+++ TP2.v
@@ -9,24 +9,56 @@ module Decodificador(
     always @(posedge clk, Controle) begin
         if (Reset)
             Saida = 4'b0000;
-        case (Saida)
-            4'b0000: case (Entrada)
-                        C1: Saida = 4'b0001;
-                        C2: Saida = 4'b0010;
-                        C1: Saida = 4'b0011;
-                        C4: Saida = 4'b0100;
-                        C5: Saida = 4'b0101;
+        else begin    
+            case (Saida)
+                4'b0000:case (Entrada)
+                            C1: Saida = 4'b0001;
+                            C2: Saida = 4'b0010;
+                            C1: Saida = 4'b0011;
+                            C4: Saida = 4'b0100;
+                            C5: Saida = 4'b0101;
                         endcase 
-            4'b0001: case (Entrada)
-                        C2: Saida = 4'b0010;
-                        C6: Saida = 4'b1001;
-                        default: Saida = 1000;
+                4'b0001:case (Entrada)
+                            C2: Saida = 4'b0010;
+                            C6: Saida = 4'b1001;
+                            C3: Saida = 4'b1000;
+                            C4: Saida = 4'b1000;
+                            C5: Saida = 4'b1000;
+                            C7: Saida = 4'b1000;
                         endcase
-            4'b0010:
-                        
-            4'b0011:
-            4'b0100:
-            4'b0101: 
-        endcase
+                4'b0010:case (Entrada)
+                            C1: Saida = 4'b0001;
+                            C3: Saida = 4'b0011;
+                            C6: Saida = 4'b1001;
+                            C4: Saida = 4'b1000;
+                            C5: Saida = 4'b1000;
+                            C7: Saida = 4'b1000;  
+                        endcase
+                4'b0011:case (Entrada)
+                            C2: Saida = 4'b0010;
+                            C4: Saida = 4'b0100;
+                            C6: Saida = 4'b1001;
+                            C1: Saida = 4'b1000;
+                            C5: Saida = 4'b1000;
+                            C7: Saida = 4'b1000;
+                        endcase
+                4'b0100:case (Entrada)
+                            C3: Saida = 4'b0011;
+                            C5: Saida = 4'b0101;
+                            C8: Saida = 4'b1010;
+                            C1: Saida = 4'b1000;
+                            C2: Saida = 4'b1000;
+                            C7: Saida = 4'b1000;
+                        endcase    
+                4'b0101:case (Entrada)
+                            C4: Saida = 4'b0100;
+                            C8: Saida = 4'b1010;
+                            C1: Saida = 4'b1000;
+                            C2: Saida = 4'b1000;
+                            C3: Saida = 4'b1000;
+                            C7: Saida = 4'b1000;
+                        endcase
+            endcase
+        end    
     end
 endmodule
